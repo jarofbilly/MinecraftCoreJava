bee6368f1015b20be9e06fed2f8e958ec9eb5278
javaless <officialvortexseven@gmail.com>
2020-04-19 14:55:51 +0100

    Changed example command to a speed selection window and merged with topy and up command

--- src/main/java/billyjar/MinecraftCore/CoreCommandManager.java
+++ src/main/java/billyjar/MinecraftCore/CoreCommandManager.java
@@ -1,13 +1,15 @@
 package billyjar.MinecraftCore;
 
-import billyjar.MinecraftCore.Commands.example;
-import billyjar.MinecraftCore.Commands.up;
+import billyjar.MinecraftCore.Commands.speed;
+import billyjar.MinecraftCore.Commands.top;
+import billyjar.MinecraftCore.Commands.upy;
 import cn.nukkit.command.CommandMap;
 
 public class CoreCommandManager {
     public static void registerAll(CoreApi api) {
         CommandMap map = api.getServer().getCommandMap();
-        map.register("CoreApi", new up(api));  //CassFile() //how to register commands
-        map.register("CoreApi", new example(api));
+        map.register("CoreApi", new upy(api));  //CassFile() //how to register commands
+        map.register("CoreApi", new speed(api));
+        map.register("CoreApi", new top(api));
     }
 }
